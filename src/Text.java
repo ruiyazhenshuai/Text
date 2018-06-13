f42057e1286e8380112259ed73e073592c8be8dc
Laybuy <Laybuy@DESKTOP-K1A980C>
2018-06-13 20:10:50 +0800

    修改过的

--- src/Text.java
+++ src/Text.java
@@ -3,7 +3,7 @@ public class Text {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("²âÊÔÊ¾Àı");
+		System.out.println("²âÊÔÊ¾Àı Hello Word");
 	}
 
 }
